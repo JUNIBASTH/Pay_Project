5d6e22bda4aba18d38d8ca92ea9883e1a2f37671
JUNIBASTH <juniorv.rojas25@gmail.com>
2025-03-31 22:02:59 -0600

    adding styles 2

--- backend/routes/employeeRoutes.ts
+++ backend/routes/employeeRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createEmployee, getAllEmployees } from '../controllers/employeeController';
+import { verifyToken, isAdmin } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
-router.get('/employees', getAllEmployees);
-router.post('/employees', createEmployee);
+router.get('/', verifyToken, getAllEmployees);
+router.post('/', verifyToken, isAdmin, createEmployee);
 
 export default router;
